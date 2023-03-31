844749aefbaf659465e5ce88182b07ccd78fac6e
TacioPatez <taciopatez@hotmail.com>
2023-03-31 15:33:11 -0300

    final

--- projetos/tasks/src/screens/Auth.js
+++ projetos/tasks/src/screens/Auth.js
@@ -1,24 +1,61 @@
 import React, { Component } from 'react'
-import { ImageBackground, Text, StyleSheet, View, TextInput, TouchableOpacity, Platform, Alert } from 'react-native'
+import { ImageBackground, Text, StyleSheet, View, TouchableOpacity, Alert } from 'react-native'
+import axios from 'axios'
 
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
+import AuthInput from '../components/AuthInput'
+import { server, showError, showSucess } from '../common'
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    stageNew: false
+}
 
 export default class Auth extends Component{
 
     state = {
-        name: '',
-        email: '',
-        password: '',
-        confirmPassword: '',
-        stageNew: true
+        ...initialState
     }
 
     signinOrSignup = () => {
         if(this.state.stageNew) {
-            Alert.alert('Criar conta', 'Sucesso!')
+            this.signup()
         } else {
-            Alert.alert('Logar', 'Sucesso!')
+            this.signin()
+        }
+    }
+
+    signup = async () => {
+        try{
+            await axios.post(`${server}/signup`,{
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword
+            })
+
+            showSucess('Usuário cadastrado!')
+            this.setState({...initialState})
+        }catch(e){
+            showError(e)
+        }
+    }
+
+    signin = async () => {
+        try{
+            const res = await axios.post(`${server}/signin`, {
+                email: this.state.email,
+                password: this.state.password
+            })
+
+            axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
+            this.props.navigation.navigate('Home')
+        }catch(e){
+            showError(e)
         }
     }
 
@@ -31,18 +68,18 @@ export default class Auth extends Component{
                         {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados'}
                     </Text>
                     {this.state.stageNew && 
-                    <TextInput placeholder='Nome' value={this.state.name}
+                    <AuthInput icon='user' placeholder='Nome' value={this.state.name}
                                 style={styles.input} 
                                 onChangeText={name => this.setState({ name })} />
                     }
-                    <TextInput placeholder='E-mail' value={this.state.email}
+                    <AuthInput icon='at' placeholder='E-mail' value={this.state.email}
                                 style={styles.input} 
                                 onChangeText={email => this.setState({ email })} />
-                    <TextInput placeholder='Senha' value={this.state.password}
+                    <AuthInput icon='lock' placeholder='Senha' value={this.state.password}
                                 style={styles.input} secureTextEntry={true}
                                 onChangeText={password => this.setState({ password })} />
                     {this.state.stageNew && 
-                        <TextInput placeholder='Confirmação de Senha' value={this.state.confirmPassword}
+                        <AuthInput icon='asterisk' placeholder='Confirmação de Senha' value={this.state.confirmPassword}
                         style={styles.input} secureTextEntry={true}
                         onChangeText={confirmPassword => this.setState({ confirmPassword })} />
                     }
@@ -93,13 +130,13 @@ const styles = StyleSheet.create({
     input: {
         backgroundColor: '#FFF',
         marginTop: 10,
-        padding: Platform.OS == 'ios'? 15 : 10
     },
     button: {
         backgroundColor: '#080',
         marginTop: 10,
         padding: 10,
         alignItems: 'center',
+        borderRadius: 10
     },
     buttonText: {
         fontFamily: commonStyles.fontFamily,
